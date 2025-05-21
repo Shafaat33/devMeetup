e3ac6c50874a749b3317c4091345bd24c6c2f11c
shafaat33 <shafaat@wanclouds.net>
2025-05-21 09:22:42 +0500

    recieve request api written

--- src/routes/requestRouter.js
+++ src/routes/requestRouter.js
@@ -61,4 +61,35 @@ requestRouter.post('/request/send/:status/:toUserId', userAuth, async (req, res)
   }
 });
 
+requestRouter.post('/request/receive/:status/:requestId', userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const { status, requestId } = req.params;
+    
+    const allowedStatus = ['accepted', 'rejected'];
+    if (!allowedStatus.includes(status)) {
+      return res.status(400).send({ message: "Status not valid!" });
+    }
+    
+    const connectionRequest = await ConnectionRequest.findOne({
+      _id: requestId,
+      toUserId: loggedInUser._id,
+      status: 'interested',
+    });
+    
+    if (!connectionRequest) {
+      return res.status(400).send({ message: "Invalid Connection Request" });
+    }
+    
+    connectionRequest.status = status;
+    
+    const data = await connectionRequest.save();
+    
+    res.status(200).json({ message: "Request Accepted Successfully", data });
+    
+  } catch (error) {
+    res.status(400).send({ message: "Something went wrong while accepting request"});
+  }
+});
+
 module.exports = requestRouter;
